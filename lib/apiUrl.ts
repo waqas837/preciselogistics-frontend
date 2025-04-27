6e2df2eee425fb401938ad3b4ff5de05d4e38019
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-04-27 16:26:11 +0500

    final v

--- lib/apiUrl.ts
+++ lib/apiUrl.ts
@@ -1,4 +1,4 @@
-export const backendUrl = "http://localhost:8000/v1"; 
-// export const backendUrl = "https://driverpwa.drawsketch.co/v1";
+// export const backendUrl = "http://localhost:8000/v1"; 
+export const backendUrl = "https://driverpwa.drawsketch.co/v1";
 // export const backendUrl = "https://api.preciselogistics.net/v1";
  
